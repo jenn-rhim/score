e46c4d364210fa27b863c598669a1316076d5bd1
Jenn Rhim <jennrhim@jenn-rhim-fluidigm.local>
2016-11-15 19:27:49 -0800

    fixed https link

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
 // add by 5
   $("#increase-5").on("click", function() {
     currentScore = currentScore + 5;
+    // score += 5;
     $("#score").html(currentScore);
+    // + " " + "Points"
   });
 
 // subtract by 5
@@ -29,8 +31,11 @@ $(document).ready(function(){
 
 
 // set custom value
-  $("#set-score").on("click", function() {
-        customScore = parseInt($("#custom-score").val());
+  $("#set-score").on("submit", function(event) {
+    event.preventDefault();
+      var  customScore = parseInt($("#custom-score").val());
+      // var customScore = $("#custom-score").val();
+      // var customScoreInt = parseInt(customScore);
         if (customScore >= 0) {
         currentScore = customScore;
         $("#score").html(currentScore);
@@ -38,3 +43,15 @@ $(document).ready(function(){
   });
 
 });
+
+
+// if (customScoreInt >= 0) {
+//   score =customScoreInt;
+//   $(#score).html(score + " Points");
+//   $("#custom-score").val("");
+// } else {
+//   score = 0;
+//   $(#score).html("0 Points");
+//   alert("You can't enter a negative score!");
+// }
+// });
